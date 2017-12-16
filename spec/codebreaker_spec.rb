b40c73f54f2c7b87a0064e3ef803670a46ad0993
Iskander Broere <iskanderbroere@gmail.com>
2017-12-16 14:03:03 +0100

    test fixes

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -158,14 +158,14 @@ RSpec.describe Codebreaker do
       it "1 exact match, 1 number match and 1 exact match (in that order) sends a mark with '++-'" do
         game.start(secret_number)
         input = '5760'
-        expect(output).to receive(:puts).with('++-')
+        expect(output).to receive(:puts).with('+--')
 
         game.guess(input)
       end
 
       it "1 number match and 2 exact matches (in that order) sends a mark with '++-'" do
         game.start(secret_number)
-        input = '48670'
+        input = '8670'
         expect(output).to receive(:puts).with('++-')
 
         game.guess(input)
